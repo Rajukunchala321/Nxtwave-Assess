b0c9db78251a648156cf5bea1e6c184b195ae130
raju <rajukunchla321@gmail>
2025-09-08 18:21:31 +0530

    assignment page done

--- Nxt Assess/my-app/src/Components/attemptQuestionComponent.js
+++ Nxt Assess/my-app/src/Components/attemptQuestionComponent.js
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {Context} from './provider'
 
 export const AttemptQuestionComponent = () => {
+  const {apiState, dispatch, state, dispatchTwo}= useContext(Context);
+  
+  
+    const handleCurrentBtn = (index)=>{
+      console.log(index);
+      dispatchTwo({type:"GOTO_NXT_QUESTION", index:index})
+
+    }
+  
   return (
     <div className='attempt-container'>
       <div className='attempt-question-container'>
         <div className='answer-count-container'>
-          <div className='ans-count'>2</div>
+          <div className='ans-count'>{state.answered.length}</div>
           <div className='ans-txt'>Answered Questions</div>
         </div>
         <div className='answer-count-container'>
-          <div className='question-count'>2</div>
+          <div className='question-count'>{(apiState.data.total) - (state.answered.length)}</div>
           <div className='ques-txt'>Unanswered Questions</div>
         </div>
       </div>
       <div className='questions'>
         <div>
-          <div className='question-txt'>Questions (10)</div>
+          <div className='question-txt'>Questions ({apiState.data.total})</div>
           <div className='question-num-container'>
-            <div className='number-btn current'>1</div>
-            <div className='number-btn submited'>2</div>
-            <div className='number-btn'>3</div>
+            {apiState.data.questions?.map((_, index)=>{ 
+             const isActive =  state.currentQuestion ===(index)
+             const isAnswered = state.answered[(index)] !==undefined;
+              return(<div onClick={()=>handleCurrentBtn(index)}  key={index} className={`number-btn ${isActive ? "current": ''} ${isAnswered ? "submited":""}`}>{index+1}</div>)
+            }
+              )}
           </div>
         </div>
         <button className='submit-btn'>Submit Assessment</button>
--- Nxt Assess/my-app/src/Components/questionAnswer.js
+++ Nxt Assess/my-app/src/Components/questionAnswer.js
@@ -1,45 +1,66 @@
-import React, {useContext} from "react";
-import './index.css'
-import {Context} from './provider'
+import React, { useContext, useState } from "react";
+import "./index.css";
+import { Context } from "./provider";
 const QuestionAnswer = () => {
-  const {apiState,state } = useContext(Context);
+  const { apiState, state } = useContext(Context);
+  const [selectOption, setSelectOption] = useState(null)
   console.log(apiState.data.questions);
   // console.log(apiState.data)
-  const questions = apiState.data.questions
+  const questions = apiState.data.questions;
 
-  const question = questions?.[state.currentQuestion]
+  const question = questions?.[state.currentQuestion];
+  console.log("state.currentQuestion", state.currentQuestion);
 
-  if (!question){
-    return(<div>Loading pls wait....</div>)
+  if (!question) {
+    return <div>Loading pls wait....</div>;
   }
 
-  return(
-     <div>
+
+  return (
+    <div>
       <h2>{question.question_text}</h2>
-      <div className="answer-container"> 
-        {question.options_type ==="DEFAULT"  ?(<div className="default-options-container">
-          <div value='true' className="default-option">Facebook</div>
-          <div className="default-option">Facebook</div>
-          <div className="default-option">Facebook</div>
-          <div className="default-option">Facebook</div>
-        </div>):(<p>Somthing went wrong</p>)}
-        {question.options_type === "IMAGE"?(<div className="default-options-container">
-          <img value='true' className="option-img" src="../img-option.png" alt="option img"/>
-          <img className="option-img" src="../img-option.png" alt="option img"/>
-          <img className="option-img" src="../img-option.png" alt="option img"/>
-           <img className="option-img" src="../img-option.png" alt="option img"/>
-        </div>):(<p>Somthing went wrong</p>)}
-        {question.options_type ==="SINGLE_SELECT" ?( <select className="select">
-             <option value='true'  >Select 1</option>
-             <option>Select 1</option>
-             <option>Select 1</option>
-          </select>):(<p>Somthing went wrong</p>)}
+      <div className="answer-container">
+        {question.options_type === "DEFAULT" && (
+          <div className="default-options-container">
+            {question.options?.map((option, idx) => (
+              <div
+                key={idx}
+                id={option.id}
+                value={option.is_correct}
+                className={`default-option ${selectOption===idx ? "selected":""}`}
+                onClick={()=>setSelectOption(idx)} 
+              >
+                {option.text}
+              </div>
+            ))}
+          </div>
+        )}
+        {question.options_type === "IMAGE" && (
+          <div className="default-options-container">
+            {question.options?.map((option, idx) => (
+              <img
+                key={idx}
+                id={option.id}
+                value={option.is_correct}
+                className={`option-img ${selectOption ===idx ? "selected":"" }`}
+                src={option.image_url}
+                alt={`option-img-${option.id}`}
+                onClick={()=>setSelectOption(idx)}
+              />
+            ))}
+          </div>
+        )}
+        {question.options_type === "SINGLE_SELECT" && (
+          <select  className="select">
+            {question.options?.map((option, idx) => (
+              <option onClick={()=>setSelectOption(idx)}  key={option.id} value={option.is_correct}>
+                {option.text}
+              </option>
+            ))}
+          </select>
+        )}
       </div>
     </div>
-  )
+  );
 };
-export default QuestionAnswer
-
-
-
- 
+export default QuestionAnswer;
